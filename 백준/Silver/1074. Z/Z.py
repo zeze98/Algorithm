b991dfc4d9699212f13facffce4b5ef2e45a6e49
zeze98 <jaewoo98@kakao.com>
2023-01-21 16:09:03 +0900

    Update Z.py

--- 백준/Silver/1074. Z/Z.py
+++ 백준/Silver/1074. Z/Z.py
@@ -1,9 +1,10 @@
 import sys
 
-
 input = sys.stdin.readline
 
 N, r, c = map(int, input().split())
+
+Max_N = 2 ** N
 
 ans = 0
 
@@ -11,20 +12,20 @@ while N != 0:
 
 	N -= 1
 
-	if r < 2 ** N and c < 2 ** N:
-		ans += ( 2 ** N ) * ( 2 ** N ) * 0
+	if r < Max_N and c < Max_N:
+		ans += ( Max_N ) * ( Max_N ) * 0
 
-	elif r < 2 ** N and c >= 2 ** N: 
-		ans += ( 2 ** N ) * ( 2 ** N ) * 1
-		c -= ( 2 ** N )
+	elif r < Max_N and c >= Max_N: 
+		ans += ( Max_N ) * ( Max_N ) * 1
+		c -= ( Max_N )
 
-	elif r >= 2 ** N and c < 2 ** N: 
-		ans += ( 2 ** N ) * ( 2 ** N ) * 2
-		r -= ( 2 ** N )
+	elif r >= Max_N and c < Max_N: 
+		ans += ( Max_N ) * ( Max_N ) * 2
+		r -= ( Max_N )
  
 	else:
-		ans += ( 2 ** N ) * ( 2 ** N ) * 3
-		r -= ( 2 ** N )
-		c -= ( 2 ** N )
+		ans += ( Max_N ) * ( Max_N ) * 3
+		r -= ( Max_N )
+		c -= ( Max_N )
     
 print(ans)
