85d907c4ad0faf806523f3f96d3c1aec50a99825
재우 <78395435+zeze98@users.noreply.github.com>
2023-03-31 15:14:50 +0900

    [level 1] Title: 폰켓몬, Time: 0.18 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
@@ -1,10 +1,3 @@
 def solution(nums):
-    answer = 0
-    pick = len(nums)//2
-    take = set(nums)
-    if len(take) >= pick:
-        answer = pick
-    else:
-        answer = len(take)
-    
+    answer = min(len(nums)/2, len(set(nums)))
     return answer
