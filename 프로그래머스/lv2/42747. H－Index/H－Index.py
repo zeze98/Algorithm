ff8df59f68b9e0653261e7f132545442c92f1031
재우 <78395435+zeze98@users.noreply.github.com>
2023-04-11 20:31:16 +0900

    [level 2] Title: H-Index, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42747. H－Index/H－Index.py
@@ -0,0 +1,7 @@
+def solution(citations):
+    answer = 0
+    citations.sort()
+    for i in range(len(citations)):
+        if citations[i] >= len(citations) - i:
+            answer += 1
+    return answer
