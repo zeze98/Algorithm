4bac89aeeb4c3a977da3824c0512a032abc7e22a
여재우 <78395435+zeze98@users.noreply.github.com>
2023-07-03 16:36:29 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.19 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
@@ -1,17 +1,26 @@
 def solution(survey, choices):
     answer = ''
-    temp = {'R': 0, 'T': 0, 'C': 0, 'F': 0, 'J': 0, 'M': 0, 'A': 0, 'N': 0}
-    for i in range(len(survey)):
-        if choices[i] <= 3:
-            temp[survey[i][0]] += (4-choices[i])
-        elif choices[i] == 4:
-            continue
-        elif choices[i] >= 5:
-            temp[survey[i][1]] += (choices[i]-4)
-    key_temp = list(temp)
-    for i in range(0, len(temp.keys()), 2):
-        if temp[key_temp[i]] >= temp[key_temp[i+1]]:
-            answer += key_temp[i]
+    
+    # 각 성격 들의 점수를 담을 수 있는 딕셔너리
+    personality = {'R': 0, 'T': 0, 'C': 0, 'F': 0, 'J': 0, 'M': 0, 'A': 0, 'N': 0}
+    
+    # 질문과 답볍의 수
+    n = len(choices)
+    
+    # 질문 별로 점수를 책정하여 딕셔너리에 저장하기
+    for i in range(n):
+        if choices[i] <= 4:
+            personality[survey[i][0]] += (4 - choices[i])
         else:
-            answer += key_temp[i+1]
+            personality[survey[i][1]] += (choices[i] - 4)
+    
+    # 키를 슬라이싱 하기 위해서 리스트로 변환
+    person_key = list(personality.keys())
+    
+    # 리스트를 2씩 보면서 i 와 i+1 비교 후 큰쪽 Answer에 넣기
+    for i in range(0, len(person_key),2):
+        if personality[person_key[i]] >= personality[person_key[i+1]]:
+            answer += person_key[i]
+        else:
+            answer += person_key[i+1]
     return answer
