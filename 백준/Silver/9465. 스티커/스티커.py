229e5d979554d4a2d0cfe43cb24567b284c201db
여재우 <78395435+zeze98@users.noreply.github.com>
2023-08-06 23:41:24 +0900

    [Silver I] Title: 스티커, Time: 772 ms, Memory: 47612 KB -BaekjoonHub

---
+++ 백준/Silver/9465. 스티커/스티커.py
@@ -0,0 +1,16 @@
+import sys
+
+input = sys.stdin.readline
+
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    sticker = [list(map(int, input().split())) for _ in range(2)]
+    if n > 1:
+        sticker[0][1] += sticker[1][0]
+        sticker[1][1] += sticker[0][0]
+    for i in range(2, n):
+        sticker[0][i] += max(sticker[1][i-1], sticker[1][i-2])
+        sticker[1][i] += max(sticker[0][i-1], sticker[0][i-2])
+    print(max(sticker[0][-1], sticker[1][-1]))
