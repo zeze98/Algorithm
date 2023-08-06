2c1f859f01254356962049eb7b3895ed51fdc03b
여재우 <78395435+zeze98@users.noreply.github.com>
2023-08-07 00:01:17 +0900

    [Silver I] Title: 곱셈, Time: 60 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1629. 곱셈/곱셈.py
@@ -0,0 +1,17 @@
+import sys
+
+input = sys.stdin.readline
+
+a, b, c = map(int, input().split())
+
+def multi(a, n):
+    if n == 1:
+        return a % c
+    else:
+        temp = multi(a, n//2)
+        if n % 2 == 0:
+            return (temp * temp) % c
+        else:
+            return (temp * temp * a) % c
+        
+print(multi(a, b))
