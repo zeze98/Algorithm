7ab36bca5077bc02439ec9c8d0a2f9b2e70e68cf
여재우 <78395435+zeze98@users.noreply.github.com>
2023-08-16 21:19:58 +0900

    [Silver III] Title: 1로 만들기, Time: 556 ms, Memory: 39068 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -2,14 +2,15 @@ import sys
 
 input = sys.stdin.readline
 
-T = int(input())
-ans = [0]*(T + 1)
+n = int(input())
+answer = [0] * (n+1)
 
-for i in range(2,T+1):
-    ans[i] = ans[i-1] + 1
+for i in range(2, n+1):
+    answer[i] = answer[i-1]+1
+
     if i % 3 == 0:
-        ans[i] = min(ans[i], ans[i//3]+1)
+        answer[i] = min(answer[i], answer[i//3]+1)
     if i % 2 == 0:
-        ans[i] = min(ans[i], ans[i//2]+1)
+        answer[i] = min(answer[i], answer[i//2]+1)
 
-print(ans[T])
+print(answer[n])
