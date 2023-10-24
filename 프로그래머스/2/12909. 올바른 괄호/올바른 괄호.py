3bcbf34d3e41f0deb5ccd34037835dbacff9c2f9
여재우 <78395435+zeze98@users.noreply.github.com>
2023-10-24 18:12:45 +0900

    [level 2] Title: 올바른 괄호, Time: 13.30 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -0,0 +1,15 @@
+def solution(s):
+    answer = True
+    temp = []
+    for i in s:
+        if i == ')' and len(temp) == 0:
+            answer = False
+            return answer
+        elif i == '(':
+            temp.append(i)
+        elif i ==')':
+            if temp[-1] == '(':
+                temp.pop()
+    if len(temp) > 0:
+        answer = False
+    return answer
