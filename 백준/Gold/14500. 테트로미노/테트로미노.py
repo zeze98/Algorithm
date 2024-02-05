295e6a1f7057908eb3504b148760404abb61b71c
여재우 <78395435+zeze98@users.noreply.github.com>
2024-02-05 18:57:06 +0900

    [Gold IV] Title: 테트로미노, Time: 176 ms, Memory: 37996 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.py
+++ 백준/Gold/14500. 테트로미노/테트로미노.py
@@ -2,40 +2,40 @@ import sys
 
 input = sys.stdin.readline
 
-
-def dfs(i, j, idx, total):
-    global ans
-    if ans >= total + max_val * (3 - idx):
-        return
-    if idx == 3:
-        ans = max(ans, total)
-        return
-    else:
-        for t in range(4):
-            ny = i + dy[t]
-            nx = j + dx[t]
-            if 0 <= ny < n and 0 <= nx < m and visit[ny][nx] == 0:
-                if idx == 1:
-                    visit[ny][nx] = 1
-                    dfs(i, j, idx + 1, total + paper[ny][nx])
-                    visit[ny][nx] = 0
-                visit[ny][nx] = 1
-                dfs(ny, nx, idx + 1, total + paper[ny][nx])
-                visit[ny][nx] = 0
-
-
 n, m = map(int, input().split())
-paper = [list(map(int, input().split())) for _ in range(n)]
-visit = [([0] * m) for _ in range(n)]
-dy = [-1, 0, 1, 0]
-dx = [0, 1, 0, -1]
-ans = 0
-max_val = max(map(max, paper))
+board = [list(map(int, input().split())) for i in range(n)]
+answer = 0
+visit = [[True] * m for _ in range(n)]
 
-for i in range(n):
-    for j in range(m):
-        visit[i][j] = 1
-        dfs(i, j, 0, paper[i][j])
-        visit[i][j] = 0
+dy = [1, 0, 0, -1]
+dx = [0, 1, -1, 0]
 
-print(ans)
+
+def dfs(y, x, point, cnt):
+    global answer
+    if answer >= point + max_board*(4-cnt):
+        return
+    if cnt == 4:
+        answer = max(answer, point)
+        return
+    for i in range(4):
+        ny = y + dy[i]
+        nx = x + dx[i]
+        if 0 <= ny < n and 0 <= nx < m and visit[ny][nx]:
+            if cnt == 2:
+                visit[ny][nx] = False
+                dfs(y, x, point + board[ny][nx], cnt + 1)
+                visit[ny][nx] = True
+            visit[ny][nx] = False
+            dfs(ny, nx, point + board[ny][nx], cnt + 1)
+            visit[ny][nx] = True
+
+
+max_board = max(map(max, board))
+for y in range(n):
+    for x in range(m):
+        visit[y][x] = False
+        dfs(y, x, board[y][x], 1)
+        visit[y][x] = True
+
+print(answer)
