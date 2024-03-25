202e510390fd713db167986525064b3aab8724f1
여재우 <78395435+zeze98@users.noreply.github.com>
2024-03-25 11:49:40 +0900

    [Silver III] Title: 1, Time: 160 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/4375. 1/1.py
@@ -0,0 +1,19 @@
+import sys
+
+input = sys.stdin.readline
+
+while True:
+    try:
+        n = int(input())
+    except:
+        break
+
+    num = 1
+    cnt = 1
+    while True:
+        if num % n != 0:
+            num = num * 10 + 1
+            cnt += 1
+        else:
+            break
+    print(cnt)
