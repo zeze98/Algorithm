0457e386c3faac52256fc37781e64cd6bef044cf
여재우 <78395435+zeze98@users.noreply.github.com>
2024-05-27 11:34:32 +0900

    [level 4] Title: FrontEnd 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
@@ -0,0 +1,8 @@
+-- 코드를 작성해주세요
+SELECT d.ID, d.EMAIL, d.FIRST_NAME, d.LAST_NAME
+FROM DEVELOPERS as d, SKILLCODES as s
+where 1 = 1 
+and (d.SKILL_CODE & s.CODE) > 0
+and CATEGORY = 'Front End'
+GROUP BY ID, EMAIL, FIRST_NAME, LAST_NAME
+ORDER BY ID;
